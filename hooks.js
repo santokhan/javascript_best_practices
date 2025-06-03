06c1ffceb4c53d3b158b5dd2969bb1fb9b789e90
Santo Khan <santokhan1999@gmail.com>
2025-06-04 05:37:07 +0600

    Update hooks.js

--- hooks.js
+++ hooks.js
@@ -28,7 +28,7 @@ While React allows custom hooks to call other hooks, **overusing this pattern** 
 
 ### 🚫 What to Avoid
 
-```jsx
+```js
 function useDashboard() {
   const user = useUser();         // unrelated logic
   const posts = usePosts();       // another custom hook
@@ -44,7 +44,7 @@ function useDashboard() {
 
 ### ✅ Better Pattern
 
-```jsx
+```js
 function useUserDashboard(user) {
   const notifications = useNotify();
 
